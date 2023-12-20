f2f89cc616b38df2a69001cd247481c80693d727
titiknk <titikhofifa@gmail.com>
2023-12-20 14:22:54 +0700

    HomeScreen

--- App.js
+++ App.js
@@ -1,35 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
-import LoginForm from './components/LoginForm';
 import HomeScreen from './screen/HomeScreen';
-import Menu from './screen/Menu';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = (email, password) => {
-    // Silakan gunakan API atau penyimpanan data yang aman untuk validasi login
-    if (email === "admin@gmail.com" && password === "admin123") {
-      setIsLoggedIn(true);
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      {isLoggedIn ? <HomeScreen /> : <LoginForm onLogin={handleLogin} />}
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
+return(
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
 export default App;
+
+
