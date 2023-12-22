3e2270fdb1afabb1635c177c643b45a198e744c3
titiknk <titikhofifa@gmail.com>
2023-12-22 13:08:39 +0700

    list data & get data api

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,22 +1,70 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, SafeAreaView, ScrollView, TouchableOpacity, TextInput, Modal, StyleSheet, Text, View } from "react-native";
+import React, {useEffect, useState} from "react";
+
+
+const Item = ({name}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{name}</Text>
+  </View>
+);
 
 const HomeScreen = () => {
-    return (
-        <View style={Styles.container}>
-            <Text>Aplikasi Movie</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getDatas = async () => {
+    try {
+      const response = await fetch('https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDatas();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator style={styles.container}/>
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Item name={item.name} />}
+          keyExtractor={item => item.id}
+        />
+      )}
+    </SafeAreaView>
+  );
 }
 
-export const Styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+container: {
+  flex: 1,
+  marginTop: StatusBar.currentHeight || 0,
+},
+item: {
+  backgroundColor: '#6495ED',
+  fontFamily: 'Poppins-Medium',
+  padding: 20,
+  marginVertical: 4,
+  marginHorizontal: 16,
+},
+title: {
+  fontSize: 32,
+},
 });
 
 export default HomeScreen;
